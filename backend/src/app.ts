a3c09a6345f80d043e3cd33e384b662377995965
kushal tamang <kushaltamang0926@gmail.com>
2025-12-24 21:03:55 +0545

    D: adding mongodb database

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,6 +4,7 @@ import { PORT, SERVER_URL } from "./config/env.ts";
 import orderRoutes from "./routes/order.routes.ts";
 import adminRoute from "./routes/admin.routes.ts";
 import productRoute from "./routes/product.routes.ts";
+import connectToDatabase from "./database/mongodb.ts";
 
 const app: Express = express();
 
@@ -17,8 +18,9 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Building the backend for the e-commerce app...");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`The e-commerce app is working in the PORT ${SERVER_URL}`);
+  await connectToDatabase();
 });
 
 export default app;
--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -2,10 +2,13 @@ import { config } from "dotenv";
 
 config({
     path: `.env.${process.env.NODE_ENV || "development"}.local`,
+    quiet: true
 });
 
 // exporting environment variables
 export const NODE_ENV: string = process.env.NODE_ENV || "development";
 export const PORT: string = process.env.PORT || "5500";
 export const SERVER_URL: string = process.env.SERVER_URL || "";
+export const MONGODB_URI: string = process.env.MONGODB_URI || "";
+
 
